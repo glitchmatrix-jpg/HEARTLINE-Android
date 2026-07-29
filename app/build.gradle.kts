1897debe2240bcb609595e154e72a26b48524eea
glitchmatrix-jpg <extraextra335@gmail.com>
2026-07-29 04:57:55 -0500

    HEARTLINE v2.1: bump version and configure Room schemas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.heartline.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20000
-        versionName = "2.0.0"
+        versionCode = 20100
+        versionName = "2.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
         buildConfigField("String", "LRCLIB_BASE_URL", "\"https://lrclib.net\"")
@@ -30,7 +30,6 @@ android {
                 "proguard-rules.pro"
             )
         }
-
         debug {
             applicationIdSuffix = ".debug"
             versionNameSuffix = "-debug"
@@ -57,6 +56,10 @@ android {
 
 kotlin {
     jvmToolchain(17)
+}
+
+ksp {
+    arg("room.schemaLocation", "$projectDir/schemas")
 }
 
 dependencies {
