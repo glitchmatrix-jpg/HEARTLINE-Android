4b700a17d3e05e62fa43e4bd27c27e4cc86927c7
glitchmatrix-jpg <extraextra335@gmail.com>
2026-07-29 12:46:41 -0500

    Fix notification listener automatic reconnection

--- app/src/main/java/com/heartline/app/media/HeartlineNotificationListener.kt
+++ app/src/main/java/com/heartline/app/media/HeartlineNotificationListener.kt
@@ -1,8 +1,39 @@
 package com.heartline.app.media
 
+import android.content.ComponentName
+import android.content.Context
 import android.service.notification.NotificationListenerService
+import java.util.concurrent.atomic.AtomicBoolean
 
 class HeartlineNotificationListener : NotificationListenerService() {
-    override fun onListenerConnected() { MediaSessionRepository.refreshSessions() }
-    override fun onListenerDisconnected() { MediaSessionRepository.markPermissionMissing() }
+    companion object {
+        private val connected = AtomicBoolean(false)
+
+        val isConnected: Boolean
+            get() = connected.get()
+
+        fun requestReconnect(context: Context) {
+            runCatching {
+                requestRebind(ComponentName(context, HeartlineNotificationListener::class.java))
+            }
+        }
+    }
+
+    override fun onListenerConnected() {
+        super.onListenerConnected()
+        connected.set(true)
+        MediaSessionRepository.onListenerConnected()
+    }
+
+    override fun onListenerDisconnected() {
+        connected.set(false)
+        MediaSessionRepository.onListenerDisconnected()
+        requestReconnect(applicationContext)
+        super.onListenerDisconnected()
+    }
+
+    override fun onDestroy() {
+        connected.set(false)
+        super.onDestroy()
+    }
 }
